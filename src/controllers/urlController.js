5a8ff9302f0b36528ce617eec47021c070e58803
dishakathane <dishakathane1995@gmail.com>
2022-07-18 20:36:52 +0530

    change regex

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,5 +1,5 @@
 const urlModel = require('../models/urlModel')
-const validUrl = require('valid-url')
+
 const shortid = require('shortid')
 
 const baseUrl = 'http://localhost:3000'
@@ -11,11 +11,15 @@ const isValidRequestBody = (RequestBody) => {
 };
 
 //values in key
-const isValid = (value) => {
+const isValid = (value) => {    
     if (typeof value === "undefined" || typeof value === null) return false;
     if (typeof value === "string" && value.trim().length === 0) return false;
     return true;
 };
+
+// regex for url
+let regexUrl = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm
+
 
 //create url
 const createUrl = async (req, res) => {
@@ -26,24 +30,32 @@ const createUrl = async (req, res) => {
         if (!isValid(longUrl)) return res.status(400).send({ status: false, message: "Please provide longURL" })
 
 
-        if (!validUrl.isUri(baseUrl)) return res.status(400).json({ status: false, message: "Please enter valid Base URL" })
+       
+        if (!regexUrl.test(longUrl.trim()))  return res.status(400).send({ status: false, message: "Provide valid url longUrl in request..." })
 
 
-        if (!validUrl.isUri(longUrl)) return res.status(400).json({ status: false, message: "Please enter valid LongURL" })
-
-
-        let checkDB = await urlModel.findOne({ longUrl: longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+        // let checkDB = await urlModel.findOne({ longUrl: longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
 
         const urlCode = shortid.generate(longUrl).toLowerCase()
 
+        let checkDBUrlCode = await urlModel.findOne({ urlCode: urlCode })
+        if(checkDBUrlCode) return res.status(400).json({ status: false, message: "urlCode is needs unique" }) 
+
         const shortUrl = baseUrl + '/' + urlCode
+
+        let checkDBshortUrl = await urlModel.findOne({ shortUrl: shortUrl })
+        if(checkDBshortUrl) return res.status(400).json({ status: false, message: "shortUrl is needs unique" }) 
+
         const newUrl = { longUrl, shortUrl, urlCode }
         const short = await urlModel.create(newUrl)
+
+
         const newData = {
             longUrl: short.longUrl,
             shortUrl: short.shortUrl,
             urlCode: short.urlCode
         }
+
         return res.status(201).send({ status: true, data: newData })
 
     } catch (err) {
@@ -62,7 +74,7 @@ const getdata = async function (req, res) {
 
         let data = await urlModel.findOne({ urlCode: urlCode });
         if (!data)
-            return res.status(400).send({ status: false, msg: "No data found" });
+            return res.status(404).send({ status: false, msg: "No data found" });
         // console.log(data)
 
         return res.status(302).redirect(data.longUrl);
@@ -71,11 +83,6 @@ const getdata = async function (req, res) {
         res.status(500).send({ status: false, error: err.message });
     }
 };
-
-
-
-
-
 
 
 module.exports = { createUrl, getdata }
