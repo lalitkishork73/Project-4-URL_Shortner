37bb713f81f041069407e49e1bd5ae12559dd758
dishakathane <dishakathane1995@gmail.com>
2022-07-20 18:49:37 +0530

    caching done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,10 +1,40 @@
-//<<================== Importing Modules ======================>>//
+//--------------------------------Importing Modules--------------------------------//
 
 const urlModel = require("../models/urlModel");
 const shortid = require("shortid");
+const validUrl = require("valid-url")
+const redis = require("redis");
+const { promisify } = require("util");
 const baseUrl = "http://localhost:3000";
 
-//<<============ GLobal Validation Defined ===================>>//
+
+
+//Connect to redis
+const redisClient = redis.createClient(
+  14064,
+  "redis-14064.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("PqUQxnu63j7X0pPsTqBoNRaHtrVMDuww", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+//-------------------------------- GLobal Validation Defined--------------------------------//
 
 const isValidRequestBody = (RequestBody) => {
   if (!Object.keys(RequestBody).length > 0) return false;
@@ -18,50 +48,53 @@ const isValid = (value) => {
 };
 
 let regexUrl =
-  /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm;
+  (/^(https[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/);
 
-//<<================== API Controllers ===========================>>//
+//----------------------- API Controllers--------------------------------//
 
-//<<============ Create URL ===========================>>//
+//--------------------------------Create URL--------------------------------//
 
 const createUrl = async (req, res) => {
   try {
     let longUrl = req.body.longUrl;
 
+    //data in body
     if (!isValidRequestBody(req.body))
       return res.status(400).send({
-        status: false,
-        message: "Invalid request. Please provide url details",
+        status: false, message: "Data required in request body..."
       });
 
+    // url present
     if (!isValid(longUrl))
-      return res
-        .status(400)
-        .send({ status: false, message: "Please provide longURL" });
+      return res.status(400).send({ status: false, message: "Please provide longURL..." });
 
+    // url valid
     if (!regexUrl.test(longUrl.trim()))
-      return res.status(400).send({
-        status: false,
-        message: "Provide valid url longUrl in request...",
-      });
+      return res.status(400).send({ status: false, message: "Provide valid url longUrl in request..." });
 
+    if (!validUrl.isUri(longUrl))
+      return res.status(400).send({ status: false, message: "Provide longUrl in rvalid formate ..." });
+
+    //get from the cache
+    let cahcelongUrl = await GET_ASYNC(`${longUrl}`)
+    if (cahcelongUrl) {
+      return res.status(200).send({ satus: true, message: "Data from Redis", data: JSON.parse(cahcelongUrl) })
+    }
+
+    // create urlcode
     const urlCode = shortid.generate(longUrl).toLowerCase();
 
     let checkDBUrlCode = await urlModel.findOne({ urlCode: urlCode });
 
     if (checkDBUrlCode)
-      return res
-        .status(400)
-        .json({ status: false, message: "urlCode is needs unique...!" });
+      return res.status(400).send({ status: false, message: "urlCode is needs unique...!" });
 
     const shortUrl = baseUrl + "/" + urlCode;
 
     let checkDBshortUrl = await urlModel.findOne({ shortUrl: shortUrl });
 
     if (checkDBshortUrl)
-      return res
-        .status(400)
-        .json({ status: false, message: "shortUrl is needs unique...!" });
+      return res.status(400).send({ status: false, message: "shortUrl is needs unique...!" });
 
     const newUrl = { longUrl, shortUrl, urlCode };
 
@@ -72,38 +105,52 @@ const createUrl = async (req, res) => {
       shortUrl: short.shortUrl,
       urlCode: short.urlCode,
     };
+    //set in the cache
+    if (newData) {
+      await SET_ASYNC(`${longUrl}`, JSON.stringify(newData));
+      return res.status(201).send({ status: true, message: "Data from DB and it sets this data in Redis ", data: newData })
+    }
 
-    return res.status(201).send({ status: true, data: newData });
+
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
 
-//<<============ Get Or Redirect URL ===========================>>//
+// -------------------------------- Get Or Redirect URL --------------------------------//
 
+// get redirect uri
 const getUrl = async function (req, res) {
   try {
-    let urlCode = req.params.urlCode;
+      let urlCode = req.params.urlCode;
+     
+      let cachedShortId = await GET_ASYNC(`${urlCode}`);
 
-    if (!shortid.isValid(urlCode))
-      return res.status(400).send({
-        status: false,
-        message: "Enter valid length of shortid between 7-14 characters...!",
-      });
+      let parsedShortId=JSON.parse(cachedShortId)
 
-    let data = await urlModel.findOne({ urlCode: urlCode });
+      if (parsedShortId) return res.status(302).redirect(parsedShortId.longUrl); /*Checking Data From Cache */
+      
 
-    if (!data)
-      return res
-        .status(404)
-        .send({ status: false, message: "No data found...!" });
+      let data = await urlModel.findOne({ urlCode: urlCode });
 
-    return res.status(302).redirect(data.longUrl);
+      if (data) {
+
+          await SET_ASYNC(`${urlCode}`, JSON.stringify(data));
+          res.status(302).redirect(data.longUrl);
+          return
+
+      } else {
+
+          return res.status(404).send({ status: false, msg: "No URL Found" });
+      }
+
+      
+
   } catch (err) {
-    res.status(500).send({ status: false, message: err.message });
+      res.status(500).send({ status: false, error: err.message });
   }
 };
 
-//<<============ Exporting Modules ===============>>//
+//------------------------ Exporting Modules-------------------------//
 
 module.exports = { createUrl, getUrl };
